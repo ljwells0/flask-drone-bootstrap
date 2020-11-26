d8f4e90e3eae82e82989595a81198fd780dbcfe2
Liam Wells <liam.wells@outlook.com.au>
2020-11-26 15:46:41 +1100

    Edit to trigger Drone

--- src/server.py
+++ src/server.py
@@ -3,7 +3,7 @@ server = Flask(__name__)
 
 @server.route("/")
 def root():
-    return "Bootstraped Flask Application"
+    return "Works!"
 
 if __name__ == "__main__":
    server.run(host='0.0.0.0') 
